a9c2b62afbc621318000ddd0d62834f714d9128e
Marcio Daniel da Rosa <marciodrosa@gmail.com>
2013-08-09 13:39:30 -0300

    Changed the script, now it is smaller, to simplify the example.

--- assets/myscript.lua
+++ assets/myscript.lua
@@ -2,7 +2,7 @@
 
 -- Returns the plugin table.
 return {
-	
+
 	-- Called when a field value is changed.
 	-- The context table is represented as the following exemple:
 	-- context = {
@@ -18,24 +18,13 @@ return {
 	-- 		}
 	-- }
 	onfieldvaluechangedbyuser = function(context)
-		local text1 = context.fieldsvalues["Field one"].value
-		local text2 = context.fieldsvalues["Field two"].value
-		local text3 = ""
-		if text1 == nil then
-			text3 = "Please type something on field 1."
-		elseif text2 == nil then
-			text3 = "Please type something on field 2."
-		end
-		if text1 ~= nil and text2 ~= nil then
-			text3 = "V1: '"..text1.."' V2: '"..text2.."'."
-		end
-		-- As result, sets a new value to the field "Field three":
+		local op1 = tonumber(context.fieldsvalues["Field one"].value) or 0
+		local op2 = tonumber(context.fieldsvalues["Field two"].value) or 0
 		return {
 			newfieldvalue = {
 				field = "Field three",
-				value = text3,
+				value = op1 + op2,
 			}
 		}
 	end
-
 }
